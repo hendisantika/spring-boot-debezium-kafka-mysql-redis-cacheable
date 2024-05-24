634d22ad089a02563ae1ba8ef2717663fe06e75c
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 06:22:33 +0700

    Add kafkaListenerDebezium in KafkaConsumerConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/debezium/config/KafkaConsumerConfig.java
+++ src/main/java/id/my/hendisantika/debezium/config/KafkaConsumerConfig.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -42,6 +43,12 @@ public class KafkaConsumerConfig {
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, customizedJsonDeserializer());
 
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), customizedJsonDeserializer());
+    }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PropertyListener> kafkaListenerDebezium() {
+        ConcurrentKafkaListenerContainerFactory<String, PropertyListener> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
