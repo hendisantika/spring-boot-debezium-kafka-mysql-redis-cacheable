9d20415e61dfbbf6a8c538a162558e723b2bf6bc
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 06:17:11 +0700

    Add PropertyListener 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/debezium/request/PropertyListener.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.debezium.request;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-debezium-kafka-mysql-redis-cacheable
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/25/24
+ * Time: 06:17
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class PropertyListener {
+
+    private PropertyPayload payload;
+}
