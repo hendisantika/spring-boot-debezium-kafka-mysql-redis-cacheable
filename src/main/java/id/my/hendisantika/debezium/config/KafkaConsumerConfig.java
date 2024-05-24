f74b9243d96c854a776ef7d7deb258f595473b31
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 06:22:12 +0700

    Add consumerFactory in KafkaConsumerConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/debezium/config/KafkaConsumerConfig.java
+++ src/main/java/id/my/hendisantika/debezium/config/KafkaConsumerConfig.java
@@ -1,9 +1,18 @@
 package id.my.hendisantika.debezium.config;
 
+import id.my.hendisantika.debezium.request.PropertyListener;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +31,17 @@ import org.springframework.kafka.annotation.EnableKafka;
 public class KafkaConsumerConfig {
 
     private String kafkaBootstrap = "localhost:29092";
+
+    @Bean
+    public ConsumerFactory<String, PropertyListener> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrap);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerFactory");
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, customizedJsonDeserializer());
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), customizedJsonDeserializer());
+
+    }
 }
