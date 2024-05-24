8f05f491720132a4eea1874392232995c842668d
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 06:19:47 +0700

    Add PropertyCacheService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/debezium/service/PropertyCacheService.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.debezium.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-debezium-kafka-mysql-redis-cacheable
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/25/24
+ * Time: 06:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class PropertyCacheService {
+}
