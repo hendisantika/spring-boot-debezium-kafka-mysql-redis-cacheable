f7e71a5f079c5d80013acb54b10bd1b6c2d806a3
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 06:19:25 +0700

    Add debeziumListener in DebeziumListener 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/debezium/service/DebeziumListener.java
+++ src/main/java/id/my/hendisantika/debezium/service/DebeziumListener.java
@@ -1,7 +1,10 @@
 package id.my.hendisantika.debezium.service;
 
+import id.my.hendisantika.debezium.request.PropertyListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,4 +23,23 @@ import org.springframework.stereotype.Service;
 public class DebeziumListener {
 
     private final PropertyCacheService propertyCacheService;
+
+    @KafkaListener(topics = "property.debezium.debezium_property", containerFactory = "kafkaListenerDebezium", groupId = "kafkaListenerDebezium")
+    public void debeziumListener(@Payload(required = false) PropertyListener message) {
+        try {
+            if (message.getPayload().getOp().equals("c")) {
+                propertyCacheService.cacheProperty(message.getPayload().getAfter().getProperty_key(),
+                        message.getPayload().getAfter().getProperty_value());
+            } else if (message.getPayload().getOp().equals("u")) {
+                propertyCacheService.cacheEvict(message.getPayload().getBefore().getProperty_key());
+                propertyCacheService.cacheProperty(message.getPayload().getAfter().getProperty_key(),
+                        message.getPayload().getAfter().getProperty_value());
+            } else {
+                // do nothing
+            }
+            log.info(message.toString());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
 }
